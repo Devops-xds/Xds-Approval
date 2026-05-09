439e0e3ab81a27d710ced77f1eba5ff28f342132
Devops-xds <devops@xdsdata.com>
2026-05-09 13:50:51 +0000

    Move PV signature area down by 4 cm

--- Xds.Approval.Api/Services/Implementations/PaymentService.cs
+++ Xds.Approval.Api/Services/Implementations/PaymentService.cs
@@ -706,7 +706,7 @@ public class PaymentService : IPaymentService
                             row.ConstantItem(150).AlignRight().Text(verificationCode).SemiBold().FontSize(8.8f);
                         });
 
-                        column.Item().PaddingTop(11).Row(row =>
+                        column.Item().PaddingTop(4, Unit.Centimetre).Row(row =>
                         {
                             row.Spacing(6);
                             row.RelativeItem().Element(container => ComposeSignatureBlock(container, "Finance Manager", reviewedBy ?? "-", financeProcessing?.AuthorizedAt, headFinanceSignature));
